5fe33245fe5f4e7cae2a3435ed9ead59996ac975
Mathias <merligmann@gmail.com>
2021-11-25 22:32:23 +0100

    v

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Form",
     platforms: [
             .macOS(.v10_14),
-            .iOS(.v10),
+            .iOS(.v13),
             .tvOS(.v13)
         ],
     products: [

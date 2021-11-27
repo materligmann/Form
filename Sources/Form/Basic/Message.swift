6c65126bcb1e00461fadcbcdf0fe70af71aaf54b
Mathias <merligmann@gmail.com>
2021-11-27 08:43:27 +0100

    commit

--- Sources/Form/Basic/Message.swift
+++ Sources/Form/Basic/Message.swift
@@ -13,6 +13,13 @@ public struct Message {
     public let textField: Bool
     public let actions: [Action]?
     
+    public init(title: String, body: String, textField: Bool, actions: [Action]?) {
+        self.title = title
+        self.body = body
+        self.textField = textField
+        self.actions = actions
+    }
+    
     public static func basicErrorModel(body: String?) -> Message {
         let defaultBody = "Something went wrong"
         return Message(title: "Error", body: body ?? defaultBody, textField: false, actions: nil)

87c037d187d306d87aa265c57f3d78455b7f21e1
Mathias <merligmann@gmail.com>
2021-12-02 08:14:07 +0100

    public

--- Sources/Form/Entries/TextEntry.swift
+++ Sources/Form/Entries/TextEntry.swift
@@ -12,4 +12,14 @@ public struct TextEntry {
     let value: String?
     var action: (()-> Void)?
     var disclosure: Bool = false
+    
+    public init(placeholder: String?,
+                value: String?,
+                action: (()-> Void)?,
+                disclosure: Bool = false) {
+        self.placeholder = placeholder
+        self.value = value
+        self.action = action
+        self.disclosure = disclosure
+    }
 }

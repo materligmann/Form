8e1bf919c9f46cde8897c7740e2a0a06107cedfa
Mathias <merligmann@gmail.com>
2022-02-03 15:22:25 +0100

    commit

--- Sources/Form/Models/Persistance.swift
+++ Sources/Form/Models/Persistance.swift
@@ -8,15 +8,59 @@
 import Foundation
 
 public class Persistance {
-    public static func set<Codable>(key: String, value: Codable) {
-        UserDefaults.standard.set(value, forKey: key)
-    }
-    
-    public static func get<T>(key: String) -> T? {
-        if let obj = UserDefaults.standard.object(forKey: key) as? T {
-            return obj
+    static func get<T: Codable>(key: String) -> [T]? {
+        let decoder = JSONDecoder()
+        if let encodedTransactions = UserDefaults.standard.object(forKey: key) as? Data,
+           let questions = try? decoder.decode([T].self, from: encodedTransactions) {
+            return questions
         }
         return nil
     }
+    
+    static func save<T: Codable>(questions: [T], key: String) {
+        let encoder = JSONEncoder()
+        if let savedQuestionEncoded = try? encoder.encode(questions) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedQuestionEncoded, forKey: key)
+        }
+    }
+    
+//    public static func set<Codable>(key: String, value: Codable) {
+//        UserDefaults.standard.set(value, forKey: key)
+//    }
+//    
+//    public static func get<T>(key: String) -> T? {
+//        if let obj = UserDefaults.standard.object(forKey: key) as? T {
+//            return obj
+//        }
+//        return nil
+//    }
+//    
+//    
+//    static func saveNew<T: Codable>(obj: T, objKey: String) {
+//        let encoder = JSONEncoder()
+//        if var savedQuestions = Persistance.get<T>(key: objKey) {
+//            savedQuestions.append(question)
+//            if let savedTransactionsEncoded = try? encoder.encode(savedQuestions) {
+//                let defaults = UserDefaults.standard
+//                defaults.set(savedTransactionsEncoded, forKey: "questions")
+//            }
+//        } else {
+//            if let savedQuestionEncoded = try? encoder.encode([question]) {
+//                let defaults = UserDefaults.standard
+//                defaults.set(savedQuestionEncoded, forKey: "questions")
+//            }
+//        }
+//    }
+//    
+//    
+//    static func get<T: Codable>() -> [T]? {
+//        let decoder = JSONDecoder()
+//        if let encodedTransactions = UserDefaults.standard.object(forKey: "questions") as? Data,
+//           let questions = try? decoder.decode([T].self, from: encodedTransactions) {
+//            return questions
+//        }
+//        return nil
+//    }
     
 }

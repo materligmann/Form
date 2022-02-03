bf9049062e68b11a39d18c8b16b76ae842da1fff
Mathias <merligmann@gmail.com>
2022-02-03 15:24:51 +0100

    commit

--- Sources/Form/Models/Persistance.swift
+++ Sources/Form/Models/Persistance.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public class Persistance {
-    static func get<T: Codable>(key: String) -> [T]? {
+    public static func get<T: Codable>(key: String) -> [T]? {
         let decoder = JSONDecoder()
         if let encodedTransactions = UserDefaults.standard.object(forKey: key) as? Data,
            let questions = try? decoder.decode([T].self, from: encodedTransactions) {
@@ -17,7 +17,7 @@ public class Persistance {
         return nil
     }
     
-    static func save<T: Codable>(questions: [T], key: String) {
+    public static func save<T: Codable>(questions: [T], key: String) {
         let encoder = JSONEncoder()
         if let savedQuestionEncoded = try? encoder.encode(questions) {
             let defaults = UserDefaults.standard
@@ -28,15 +28,15 @@ public class Persistance {
 //    public static func set<Codable>(key: String, value: Codable) {
 //        UserDefaults.standard.set(value, forKey: key)
 //    }
-//    
+//
 //    public static func get<T>(key: String) -> T? {
 //        if let obj = UserDefaults.standard.object(forKey: key) as? T {
 //            return obj
 //        }
 //        return nil
 //    }
-//    
-//    
+//
+//
 //    static func saveNew<T: Codable>(obj: T, objKey: String) {
 //        let encoder = JSONEncoder()
 //        if var savedQuestions = Persistance.get<T>(key: objKey) {
@@ -52,8 +52,8 @@ public class Persistance {
 //            }
 //        }
 //    }
-//    
-//    
+//
+//
 //    static func get<T: Codable>() -> [T]? {
 //        let decoder = JSONDecoder()
 //        if let encodedTransactions = UserDefaults.standard.object(forKey: "questions") as? Data,

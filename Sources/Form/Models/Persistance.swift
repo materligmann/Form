e3073a91caf7bafdeac22f75e1aa81fed7f5387d
Mathias <merligmann@gmail.com>
2022-02-03 14:39:54 +0100

    commit

--- Sources/Form/Models/Persistance.swift
+++ Sources/Form/Models/Persistance.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public class Persistance {
-    static func register(key: String, value: Codable) {
+    public static func register(key: String, value: Codable) {
         UserDefaults.standard.set(value, forKey: key)
     }
 }

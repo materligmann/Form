ffe51d07de0c09492cd0bc8684e79f61109b14dc
Mathias <merligmann@gmail.com>
2022-02-06 17:47:03 +0100

    commit

--- Sources/Form/Models/Persistance.swift
+++ Sources/Form/Models/Persistance.swift
@@ -8,7 +8,24 @@
 import Foundation
 
 public class Persistance {
-    public static func get<T: Codable>(key: String) -> [T]?{
+    public static func get<T: Codable>(key: String) -> T?{
+        let decoder = JSONDecoder()
+        if let encoded = UserDefaults.standard.object(forKey: key) as? Data,
+           let objects = try? decoder.decode(T.self, from: encoded) {
+            return objects
+        }
+        return nil
+    }
+    
+    public static func save<T: Codable>(objects: T, key: String) {
+        let encoder = JSONEncoder()
+        if let savedEncoded = try? encoder.encode(objects) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedEncoded, forKey: key)
+        }
+    }
+    
+    public static func getArray<T: Codable>(key: String) -> [T]?{
         let decoder = JSONDecoder()
         if let encoded = UserDefaults.standard.object(forKey: key) as? Data,
            let objects = try? decoder.decode([T].self, from: encoded) {
@@ -17,7 +34,7 @@ public class Persistance {
         return nil
     }
     
-    public static func save<T: Codable>(objects: [T], key: String) {
+    public static func saveArray<T: Codable>(objects: [T], key: String) {
         let encoder = JSONEncoder()
         if let savedEncoded = try? encoder.encode(objects) {
             let defaults = UserDefaults.standard
@@ -25,21 +42,21 @@ public class Persistance {
         }
     }
     
-    public static func saveNew<T: Codable>(object: T, key: String) {
-        if var savedObjects: [T] = Persistance.get(key: key) {
+    public static func saveNewArray<T: Codable>(object: T, key: String) {
+        if var savedObjects: [T] = Persistance.getArray(key: key) {
             savedObjects.append(object)
-            Persistance.save(objects: savedObjects, key: key)
+            Persistance.saveArray(objects: savedObjects, key: key)
         } else {
-            Persistance.save(objects: [object], key: key)
+            Persistance.saveArray(objects: [object], key: key)
         }
     }
     
     public static func delete<T: Equatable & Codable>(deletedObject: T, key: String) {
-        if var savedObjects: [T] = Persistance.get(key: key) {
+        if var savedObjects: [T] = Persistance.getArray(key: key) {
             for (i, object) in savedObjects.enumerated() {
                 if object == deletedObject {
                     savedObjects.remove(at: i)
-                    Persistance.save(objects: savedObjects, key: key)
+                    Persistance.saveArray(objects: savedObjects, key: key)
                     return
                 }
             }

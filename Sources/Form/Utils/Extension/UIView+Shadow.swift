6eae9fcd6dee0675dd2846408927694935bce6a0
Mathias <merligmann@gmail.com>
2022-04-08 21:30:26 +0200

    drop shadow

--- Sources/Form/Utils/Extension/UIView+Shadow.swift
+++ Sources/Form/Utils/Extension/UIView+Shadow.swift
@@ -16,9 +16,9 @@ extension UIView {
     layer.shadowOffset = CGSize(width: -1, height: 1)
     layer.shadowRadius = 1
 
-    layer.shadowPath = UIBezierPath(rect: bounds).cgPath
-    layer.shouldRasterize = true
-    layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+//    layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+//    layer.shouldRasterize = true
+//    layer.rasterizationScale = scale ? UIScreen.main.scale : 1
   }
 
   public func dropShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {

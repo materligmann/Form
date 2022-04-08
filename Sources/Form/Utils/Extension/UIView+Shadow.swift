74891445ce0c440d8801eda8b20121eeb6f66d6c
Mathias <merligmann@gmail.com>
2022-04-08 21:33:00 +0200

    drop Shadow

--- Sources/Form/Utils/Extension/UIView+Shadow.swift
+++ Sources/Form/Utils/Extension/UIView+Shadow.swift
@@ -8,28 +8,24 @@
 import UIKit
 
 extension UIView {
-
-  public func dropShadow(scale: Bool = true) {
-    layer.masksToBounds = false
-    layer.shadowColor = UIColor.black.cgColor
-    layer.shadowOpacity = 0.5
-    layer.shadowOffset = CGSize(width: -1, height: 1)
-    layer.shadowRadius = 1
-
-//    layer.shadowPath = UIBezierPath(rect: bounds).cgPath
-//    layer.shouldRasterize = true
-//    layer.rasterizationScale = scale ? UIScreen.main.scale : 1
-  }
-
-  public func dropShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
-    layer.masksToBounds = false
-    layer.shadowColor = color.cgColor
-    layer.shadowOpacity = opacity
-    layer.shadowOffset = offSet
-    layer.shadowRadius = radius
-
-    layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
-    layer.shouldRasterize = true
-    layer.rasterizationScale = scale ? UIScreen.main.scale : 1
-  }
+    
+    public func dropShadow(scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor(red: 0.38, green: 0.23, blue: 0.68, alpha: 0.1).cgColor
+        layer.shadowOpacity = 1
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowRadius = 30
+    }
+    
+    public func dropShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowOffset = offSet
+        layer.shadowRadius = radius
+        
+        layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
 }

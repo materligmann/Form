b59cb77646cba4fffa9183ce561fa8ada3f2c49d
Mathias <merligmann@gmail.com>
2022-08-23 16:28:10 +0200

    change ranking cell

--- Sources/Form/Cells/RankingCell.swift
+++ Sources/Form/Cells/RankingCell.swift
@@ -10,7 +10,8 @@ import UIKit
 public class RankingCell: UITableViewCell {
     
     private let rankingLabel = UILabel()
-    private let valueLabel = UILabel()
+    private let nameLabel = UILabel()
+    private let rankingIcon = UIImageView()
     
     public class var cellIdentifier: String {
         return "RankingCell"
@@ -21,6 +22,8 @@ public class RankingCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         configureRankingLabel()
+        configureRankingIcon()
+        configureNameLabel()
     }
     
     required init?(coder: NSCoder) {
@@ -41,8 +44,30 @@ public class RankingCell: UITableViewCell {
         rankingLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor, constant: 0).isActive = true
     }
     
+    private func configureRankingIcon() {
+        rankingIcon.image = UIImage(systemName: "line.3.horizontal")
+        rankingIcon.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(rankingIcon)
+        rankingIcon.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16).isActive = true
+        rankingIcon.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        rankingIcon.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        rankingIcon.centerYAnchor.constraint(equalTo: contentView.centerYAnchor, constant: 0).isActive = true
+    }
+    
+    private func configureNameLabel() {
+        nameLabel.textAlignment = .center
+        nameLabel.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(nameLabel)
+        nameLabel.leftAnchor.constraint(equalTo: rankingLabel.rightAnchor, constant: 10).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        nameLabel.rightAnchor.constraint(equalTo: rankingIcon.leftAnchor, constant: -10).isActive = true
+        nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor, constant: 0).isActive = true
+    }
+    
     func set(entry: RankingEntry) {
         rankingLabel.backgroundColor = entry.rankingColor
         rankingLabel.text = entry.ranking
+        nameLabel.text = entry.name
     }
 }

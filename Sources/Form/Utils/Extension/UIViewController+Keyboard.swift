1f40f2bbdeeb977a174f7c87503fb122b245d1d8
Mathias <merligmann@gmail.com>
2022-09-07 01:04:33 +0200

    add public

--- Sources/Form/Utils/Extension/UIViewController+Keyboard.swift
+++ Sources/Form/Utils/Extension/UIViewController+Keyboard.swift
@@ -8,13 +8,40 @@
 import UIKit
 
 extension UIViewController {
-    public func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+    func configureKeyboardNotification(bottom: NSLayoutConstraint) {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name:UIResponder.keyboardWillShowNotification, object: bottom);
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name:UIResponder.keyboardWillHideNotification, object: bottom);
+    }
+    
+    @objc func keyboardWillShow(sender: NSNotification) {
+        if let bottom = sender.object as? NSLayoutConstraint,
+           let keyboardFrame: NSValue = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            bottom.constant = -keyboardHeight
+            UIView.animate(withDuration: 0.25) {
+                self.view.layoutSubviews()
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(sender: NSNotification) {
+        if let bottom = sender.object as? NSLayoutConstraint {
+            bottom.constant = 0
+            UIView.animate(withDuration: 0.25) {
+                self.view.layoutSubviews()
+            }
+        }
+    }
+    
+    
+    func configureKeyboard() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:    #selector(UIViewController.dismissKeyboardTouchOutside))
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
     
-    @objc func dismissKeyboard() {
+    @objc func dismissKeyboardTouchOutside() {
         view.endEditing(true)
     }
 }

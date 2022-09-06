baed567bfde3233b94d1e3d9951712c8ae71cc87
Mathias <merligmann@gmail.com>
2022-09-07 00:35:39 +0200

    add public

--- Sources/Form/Utils/Extension/UINavigationController+PopCompletion.swift
+++ Sources/Form/Utils/Extension/UINavigationController+PopCompletion.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 extension UINavigationController {
-    func popViewController(animated: Bool, completion: (() -> Void)?) {
+    public func popViewController(animated: Bool, completion: (() -> Void)?) {
         CATransaction.begin()
         CATransaction.setCompletionBlock(completion)
         self.popViewController(animated: animated)
         CATransaction.commit()
     }
     
-    func popToRootViewController(animated: Bool, completion: (() -> Void)?) {
+    public func popToRootViewController(animated: Bool, completion: (() -> Void)?) {
         CATransaction.begin()
         CATransaction.setCompletionBlock(completion)
         self.popToRootViewController(animated: animated)

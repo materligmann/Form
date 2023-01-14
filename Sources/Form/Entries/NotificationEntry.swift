bf9e5445a7b15410ce58827b89d2c1d03e7111f7
Mathias <merligmann@gmail.com>
2023-01-14 20:47:38 +0100

    add public

--- Sources/Form/Entries/NotificationEntry.swift
+++ Sources/Form/Entries/NotificationEntry.swift
@@ -10,4 +10,9 @@ import Foundation
 public struct NotificationEntry {
     let numberOfNotification: Int
     let text: String
+    
+    public init(numberOfNotification: Int, text: String) {
+        self.numberOfNotification = numberOfNotification
+        self.text = text
+    }
 }

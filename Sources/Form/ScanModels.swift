5c199ac80578912acce5af1d485d979be1328ad5
Mathias Erligmann <merligmann@gmail.com>
2023-08-24 22:29:02 +0200

    commit

--- Sources/Form/ScanModels.swift
+++ Sources/Form/ScanModels.swift
@@ -12,5 +12,10 @@ public enum ScanModels {
     public struct Request {
         var scanType: [AVMetadataObject.ObjectType]
         var onCodeFound: ((String) -> Void)?
+        
+        public init(scanType: [AVMetadataObject.ObjectType], onCodeFound: ((String) -> Void)? = nil) {
+            self.scanType = scanType
+            self.onCodeFound = onCodeFound
+        }
     }
 }

1794f0b79e02b23e1f7a7f449171bf8378c89c34
Mathias <merligmann@gmail.com>
2023-10-06 18:32:58 +0200

    commit

--- Sources/Form/Utils/Extension/ImageView+Download.swift
+++ Sources/Form/Utils/Extension/ImageView+Download.swift
@@ -14,8 +14,10 @@ extension UIImageView {
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard
                 let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
+                let mimeType = response?.mimeType,
+                mimeType.hasPrefix("image"),
+                let data = data,
+                error == nil,
                 let image = UIImage(data: data)
             else { return }
             DispatchQueue.main.async() { [weak self] in

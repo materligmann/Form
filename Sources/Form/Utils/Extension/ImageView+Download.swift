a2f7d3debca6db77f95f914389e553b86a2de361
Mathias <merligmann@gmail.com>
2023-10-06 18:27:15 +0200

    commit

---
+++ Sources/Form/Utils/Extension/ImageView+Download.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mathias Erligmann on 06/10/2023.
+//
+
+import UIKit
+
+
+extension UIImageView {
+    public func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+            else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    public func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}

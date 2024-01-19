d584627cc288a456af4960727462ff9e7ee13bd3
Mathias <merligmann@gmail.com>
2024-01-19 15:39:10 +0100

    add title cell

---
+++ Sources/Form/Cells/TitleCell.swift
@@ -0,0 +1,50 @@
+//
+//  TitleCell.swift
+//  jobs.paris
+//
+//  Created by Mathias Erligmann on 11/01/2024.
+//
+
+import UIKit
+
+public class TitleCell: UITableViewCell {
+    
+    private let titleLabel = UILabel()
+    
+    public class var cellIdentifier: String {
+        return "TitleCell"
+    }
+    
+    // MARK: Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureTitleLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Set
+    
+    func setAccessory(accessory: UITableViewCell.AccessoryType) {
+        accessoryType = accessory
+    }
+    
+    public func set(title: String) {
+        titleLabel.text = title
+    }
+    
+    private func configureTitleLabel() {
+        titleLabel.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(titleLabel)
+        titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0).isActive = true
+    }
+}

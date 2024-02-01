da12ce19c6fac48572914586f8e4e266a30c82a6
Mathias <merligmann@gmail.com>
2024-02-01 02:19:41 +0100

    update slider

--- Sources/Form/Cells/SliderCell.swift
+++ Sources/Form/Cells/SliderCell.swift
@@ -10,6 +10,8 @@ import UIKit
 public class SliderCell: UITableViewCell {
     
     private let slider = UISlider()
+    private let amountLabel = UILabel()
+    private let descriptionLabel = UILabel()
     
     public class var cellIdentifier: String {
         return "SliderCell"
@@ -21,8 +23,9 @@ public class SliderCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        backgroundColor = .backColor
         selectionStyle = .none
+        configureAmountLabel()
+        configureDescriptionLabel()
         configureSlider()
     }
     
@@ -39,9 +42,32 @@ public class SliderCell: UITableViewCell {
         slider.maximumValue = Float(entry.maximum)
         slider.setValue(Float(entry.initialValue), animated: false)
         slider.tintColor = entry.tintColor
+        amountLabel.text = String(entry.initialValue)
+        descriptionLabel.text = entry.description
     }
     
     // MARK: Configure
+    
+    private func configureAmountLabel() {
+        amountLabel.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        amountLabel.textAlignment = .right
+        amountLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(amountLabel)
+        amountLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16).isActive = true
+        amountLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        amountLabel.widthAnchor.constraint(equalToConstant: 120).isActive = true
+        amountLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+    }
+    
+    private func configureDescriptionLabel() {
+        descriptionLabel.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(descriptionLabel)
+        descriptionLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16).isActive = true
+        descriptionLabel.rightAnchor.constraint(equalTo: amountLabel.leftAnchor, constant: -10).isActive = true
+        descriptionLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10).isActive = true
+    }
     
     private func configureSlider() {
         slider.addTarget(self, action: #selector(onSliderChange), for: .valueChanged)
@@ -49,7 +75,7 @@ public class SliderCell: UITableViewCell {
         contentView.addSubview(slider)
         slider.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16).isActive = true
         slider.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16).isActive = true
-        slider.centerYAnchor.constraint(equalTo: contentView.centerYAnchor, constant: 0).isActive = true
+        slider.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 20).isActive = true
     }
     
     // MARK: User Action
@@ -63,6 +89,7 @@ public class SliderCell: UITableViewCell {
         if slider.minimumValue == slider.value {
             min = true
         }
+        amountLabel.text = String(slider.value)
         action?(slider.value, max, min)
     }
     
--- Sources/Form/Entries/SliderEntry.swift
+++ Sources/Form/Entries/SliderEntry.swift
@@ -14,16 +14,18 @@ public struct SliderEntry {
     let maximum: Float
     let tintColor: UIColor
     let isContinuous: Bool
+    let description: String
     
     public init(onSliderChange: @escaping (Float, Bool, Bool) -> Void,
                 initialValue: Float,
                 minimum: Float,
-                maximum: Float, tintColor: UIColor, isContinuous: Bool) {
+                maximum: Float, tintColor: UIColor, isContinuous: Bool, description: String) {
         self.onSliderChange = onSliderChange
         self.initialValue = initialValue
         self.minimum = minimum
         self.maximum = maximum
         self.tintColor = tintColor
         self.isContinuous = isContinuous
+        self.description = description
     }
 }

e15ab4bf000a9a71961f2a5681f87241a425230c
Mathias <merligmann@gmail.com>
2024-05-30 06:31:52 +0200

    add public

--- Sources/Form/Cells/ImageCell.swift
+++ Sources/Form/Cells/ImageCell.swift
@@ -36,7 +36,7 @@ public class ImageCell: UITableViewCell {
     
     // MARK: Set
     
-    public func set(entry: ImageEntry) {
+     public func set(entry: ImageEntry) {
         if let color = entry.color {
             iconImageView.tintColor = color
         }

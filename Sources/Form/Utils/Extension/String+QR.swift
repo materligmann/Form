1c464b31bd877a1abc2d2dcd1d8f4f2977a75850
Mathias <merligmann@gmail.com>
2025-05-15 00:43:26 +0200

    add white qr

--- Sources/Form/Utils/Extension/String+QR.swift
+++ Sources/Form/Utils/Extension/String+QR.swift
@@ -4,26 +4,26 @@ import UIKit
 extension String {
     public func generateQRCodeFromString(lightBackground: Bool = true) -> UIImage? {
         guard let data = self.data(using: .utf8),
-              let qrFilter = CIFilter(name: "CIQRCodeGenerator") else {
+              let qrFilter = CIFilter(name: "CIQRCodeGenerator"),
+              let colorFilter = CIFilter(name: "CIFalseColor") else {
             return nil
         }
 
         qrFilter.setValue(data, forKey: "inputMessage")
         qrFilter.setValue("M", forKey: "inputCorrectionLevel")
-        guard let outputImage = qrFilter.outputImage else { return nil }
+
+        guard let qrImage = qrFilter.outputImage else { return nil }
 
         // Scale it
-        let scaledQR = outputImage.transformed(by: CGAffineTransform(scaleX: 10, y: 10))
+        let transformedImage = qrImage.transformed(by: CGAffineTransform(scaleX: 10, y: 10))
 
-        // Apply color filter
-        guard let colorFilter = CIFilter(name: "CIFalseColor") else { return nil }
-
-        colorFilter.setValue(scaledQR, forKey: kCIInputImageKey)
+        // Set colors depending on background style
+        colorFilter.setValue(transformedImage, forKey: "inputImage")
         if lightBackground {
-            colorFilter.setValue(CIColor(color: .black), forKey: "inputColor0") // QR code dots
+            colorFilter.setValue(CIColor(color: .black), forKey: "inputColor0") // QR pixels
             colorFilter.setValue(CIColor(color: .white), forKey: "inputColor1") // Background
         } else {
-            colorFilter.setValue(CIColor(color: .white), forKey: "inputColor0") // QR code dots
+            colorFilter.setValue(CIColor(color: .white), forKey: "inputColor0") // QR pixels
             colorFilter.setValue(CIColor(color: .black), forKey: "inputColor1") // Background
         }
 

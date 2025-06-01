346c5eb5e80d631018b9c7a16a1ff3f356af1ee6
Mathias <merligmann@gmail.com>
2025-06-01 19:39:00 +0200

    add frame

--- Package.swift
+++ Package.swift
@@ -25,7 +25,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Form",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Resources") // this is relative to Sources/MyPackage
+            ]),
         .testTarget(
             name: "FormTests",
             dependencies: ["Form"]),

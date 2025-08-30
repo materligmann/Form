e18ee2e799f906d0738dcd3e62493f596c38e6ca
Mathias Erligmann <merligmann@gmail.com>
2025-08-30 21:51:22 +0200

    change detail font

--- Sources/Form/Entries/DetailEntry.swift
+++ Sources/Form/Entries/DetailEntry.swift
@@ -15,7 +15,7 @@ public struct DetailEntry {
     public let tintColor: UIColor
     public let font: UIFont
     
-    public init(title: String, text: String?, image: BasicImage, onAction: (() -> Void)?, tintColor: UIColor, font: UIFont = UIFont.systemFont(ofSize: 17, weight: .bold)) {
+    public init(title: String, text: String?, image: BasicImage, onAction: (() -> Void)?, tintColor: UIColor, font: UIFont = UIFont.systemFont(ofSize: 15, weight: .regular)) {
         self.title = title
         self.text = text
         self.image = image
